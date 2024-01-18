b6671bc105d9ff76a35098fabcbbe40496842079
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-01-18 17:47:43 -0600

    Trabajando con exportaciones

---
+++ 02-Bases-Nodejs/src/app.js
@@ -0,0 +1,3 @@
+const { emailTemplate } = require('./js-foundation/01-template.js')
+
+console.log( emailTemplate )
---
+++ 02-Bases-Nodejs/src/js-foundation/01-template.js
@@ -0,0 +1,12 @@
+const emailTemplate = `
+
+<div>
+  <h1> Hi, {{name}} </h1>
+  <p>Gracias por su compra</p>
+</div>
+`
+
+
+module.exports = {
+  emailTemplate
+}
