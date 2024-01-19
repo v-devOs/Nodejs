ee7037a05fe45081f8448d150c652794adefa5d1
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-01-19 13:32:13 -0600

    Aprendiendo desestrucuturacion

--- 02-Bases-Nodejs/src/app.js
+++ 02-Bases-Nodejs/src/app.js
@@ -1,3 +1,4 @@
 const { emailTemplate } = require('./js-foundation/01-template.js')
+require('./js-foundation/02-destructuring.js')
 
 console.log( emailTemplate )
---
+++ 02-Bases-Nodejs/src/js-foundation/02-destructuring.js
@@ -0,0 +1,13 @@
+console.log(process.env)
+
+const { SHELL, HOMEBREW_PREFIX } = process.env
+
+// console.table({SHELL, HOMEBREW_PREFIX})
+
+
+const characters = ['Flash', 'Superman', 'Batman']
+
+
+const [_,__, batman] = characters
+
+
