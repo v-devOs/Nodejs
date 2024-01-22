6c87762f76859c452b5291775c392bc0651b2865
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-01-22 13:03:17 -0600

    Empezando a trabajar con promesas

--- 02-Bases-Nodejs/src/app.js
+++ 02-Bases-Nodejs/src/app.js
@@ -1,11 +1,19 @@
-const { getAge, getUUID } = require('./plugins')
+/* const { getAge, getUUID } = require('./plugins')
 
-const { buildMakePerson } = require('./js-foundation/05-factory')
+const { buildMakePerson } = require('./js-foundation/05-factory') */
 
-const makePerson = buildMakePerson({ getUUID, getAge })
+/* const makePerson = buildMakePerson({ getUUID, getAge })
 
 const obj = { name: 'Jonh', birthDate: '2003-06-30' }
 
 const john = makePerson(obj)
 
-console.log(john)
+console.log(john) */
+
+const getPokemonById = require('./js-foundation/06-promises')
+
+getPokemonById(4, ( pokemon ) => {
+  console.log({ pokemon }) 
+})
+
+
