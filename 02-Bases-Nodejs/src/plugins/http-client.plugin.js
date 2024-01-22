7e5877987a33fadd740b788e6ff11f0b00df5388
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-01-22 13:55:20 -0600

    Uso de Axios

--- 02-Bases-Nodejs/src/plugins/http-client.plugin.js
+++ 02-Bases-Nodejs/src/plugins/http-client.plugin.js
@@ -1,14 +1,16 @@
 
+const axios = require('axios')
 // Todos nuestros tipos de peticiones pasaran por aqui
 const httpClientPlugin = {
   get: async( url ) => {
-    const response = await fetch(url)
-    return await response.json()
+    /* const response = await fetch(url)
+    return await response.json() */
+
+    const { data } = await axios.get(url)
+    return data
   },
 
   post: async(url, body ) => {}
-
-
 }
 
 module.exports = {
